aa808b7211d102c51835586047baa6317383d15a
Xavier Caldwell <xvrianreed@gmail.com>
2023-03-17 15:29:08 -0600

    changed key

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-99y_fc1-f-fioel7=b#f3lzvukyw*q)6_e^nwkwisto*m_678&'
-
+SECRET_KEY = 'django-insecure-l($g$b4bm@m5h---#_7+yz84olmbq)5*$xzzj+!wtz^%dj)s@b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
