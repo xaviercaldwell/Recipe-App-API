2b1cc971913ec3eb759fbce0cfbe1d7e4b0f68dc
Xavier Caldwell <xvrianreed@gmail.com>
2023-03-28 11:03:11 -0600

    addedController

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x, y):
+    """add x and y and return result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract y from x and return result"""
+    return x - y
