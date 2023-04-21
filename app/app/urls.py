e10af334f79ea18a50819881478c64ac842bb127
Xavier Caldwell <xvrianreed@gmail.com>
2023-04-21 15:33:27 -0600

    usertests

--- app/app/urls.py
+++ app/app/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
-        SpectacularSwaggerView.as_view()(url_name='api-schema'),
+        SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
         )
 ]
