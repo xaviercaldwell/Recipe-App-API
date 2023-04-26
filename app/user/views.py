0cf6fa1c964fcb798451f194d8d8627adf812426
Xavier Caldwell <xvrianreed@gmail.com>
2023-04-26 10:00:55 -0600

    added token auth

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+"""
+Views for the user API
+"""
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+    )
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system."""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Creates a new auth token for user."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
