7dd8bb02ee8bb76fa56c907bb2c61e0c8b2a4ae2
Xavier Caldwell <xvrianreed@gmail.com>
2023-05-10 10:33:19 -0600

    tag api added

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,7 +3,7 @@ Serializers for recipe APIs
 """
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (Recipe, Tag)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -12,4 +12,20 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for recipe detail view"""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """serializer for tags"""
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
         read_only_fields = ['id']
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,17 +1,17 @@
 """
 Views for the recipe APIs
 """
-from rest_framework import viewsets
+from rest_framework import (viewsets, mixins)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (Recipe, Tag)
 from recipe import serializers
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """view for manage recipe APIs"""
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
@@ -19,3 +19,26 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """return the serializer class for request"""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """create a new recipe"""
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage Tags in the database"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """filter queryset to authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
